3a325c9104bb94e69db0bca398ad24c0ceb0a2fa
IMYdev <121319594+IMYdev@users.noreply.github.com>
2025-02-03 20:59:56 +0200

    diz causing conflicts with the sprd-common?

--- common.mk
+++ common.mk
@@ -81,8 +81,8 @@ PRODUCT_PACKAGES += \
 	libHWCUtils \
 	gralloc.sc8830 \
 	hwcomposer.sc8830 \
-	sprd_gsp.sc8830 \
-	libion_sprd
+	sprd_gsp.sc8830
+	#libion_sprd
 
 # System init.rc files
 PRODUCT_PACKAGES += \
